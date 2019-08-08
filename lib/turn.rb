7b08152bbec05ba27b92fe782e359db05cce845f
Oliver Parkinson <olliepar01@gmail.com>
2019-08-08 14:20:37 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -27,9 +27,11 @@ end
 def move(board, index, token="X") 
   board[index] = token
 end
+
 def input_to_index(index) 
   index = index.to_i - 1
 end 
+
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
